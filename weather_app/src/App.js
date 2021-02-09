76354834d1321e07c3439d27d3e91a5e09214e99
bigchsta62 <65703232+bigchsta62@users.noreply.github.com>
2021-02-09 12:03:56 -0700

    begin react refactor

--- weather_app/src/App.js
+++ weather_app/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import WeatherCard from "./Components/WeatherCard"
+import Nav from "./Components/Nav"
 
 function App() {
   return (
     <div className="App">
-      
+      <Nav/>
       <WeatherCard/>
     </div>
   );
