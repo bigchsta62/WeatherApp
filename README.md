f1765c4f9fbc5d1ab10bc9d69d049c78273cdea2
bigchsta62 <65703232+bigchsta62@users.noreply.github.com>
2021-03-06 13:23:33 -0700

    removed numbers api due to cross origin errors

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # News and Weather APP
 
 ### This app was created to provide a 5 day forecast along with some news headlines
-### It also includes a fun fact about "today in history"
+
 
--- jscript.js
+++ jscript.js
@@ -1,11 +1,8 @@
 $(document).ready(function () {
   const weatherKey = "1893546eadda6ea230333e67a557c549";
   const date = moment().format("MMMM Do YYYY");
-  console.log(date);
   const month = moment().format("MM");
   const day = moment().format("DD");
-  console.log(month, day);
-
 
   //Numbers API used for This Day in History section
   const historyURL = "http://numbersapi.com/" + month + "/" + day + "/date";
@@ -14,9 +11,12 @@ $(document).ready(function () {
   let fivedayURL = "";
   let basicURL = "";
 
-  $.get('http://numbersapi.com/' + month + "/" + day + '/date', function(data) {
-    $('#number').text(data);
-});
+  $.get(
+    "http://numbersapi.com/" + month + "/" + day + "/date",
+    function (data) {
+      $("#number").text(data);
+    }
+  );
 
   function basicData(location) {
     basicURL =
@@ -40,12 +40,11 @@ $(document).ready(function () {
       url: basicURL,
       method: "GET",
     }).then(function (cityData) {
-      console.log(cityData);
       lat = cityData.coord.lat;
       long = cityData.coord.lon;
-      console.log("current coords", lat, long);
+
       fiveDay();
-      console.log("fiveDayUrl", fivedayURL);
+
       const unix = cityData.dt;
       const milli = unix * 1000;
       const date = new Date(milli);
@@ -65,13 +64,10 @@ $(document).ready(function () {
         url: fivedayURL,
         method: "GET",
       }).then(function (onecall) {
-        console.log(onecall);
         for (let i = 0; i < 6; i++) {
           $("#day" + i).empty();
-          console.log(i);
-          const todayIcon = onecall.daily[i].weather[0].icon;
-          console.log(todayIcon);
 
+          const todayIcon = onecall.daily[i].weather[0].icon;
           const unix = onecall.daily[i].dt;
           const milli = unix * 1000;
           const date = new Date(milli);
@@ -89,7 +85,7 @@ $(document).ready(function () {
           const weatherIcon = $("<img align='right'/>");
           weatherIcon.attr(
             "src",
-            "https://openweathermap.org/img/w/" + todayIcon + ".png" 
+            "https://openweathermap.org/img/w/" + todayIcon + ".png"
           );
           weatherIcon.html("<span>" + humanizedLong + "</span>");
 
@@ -100,22 +96,22 @@ $(document).ready(function () {
           const morn = $('<li class="font-weight-normal">');
           morn.text(
             "Morning: " +
-            Math.floor((onecall.daily[i].temp.morn - 273.15) * 1.8 + 32) +
-            "°F"
+              Math.floor((onecall.daily[i].temp.morn - 273.15) * 1.8 + 32) +
+              "°F"
           );
 
           const day = $('<li class="font-weight-normal">');
           day.text(
             "Day: " +
-            Math.floor((onecall.daily[i].temp.day - 273.15) * 1.8 + 32) +
-            "°F"
+              Math.floor((onecall.daily[i].temp.day - 273.15) * 1.8 + 32) +
+              "°F"
           );
 
           const eve = $('<li class="font-weight-normal">');
           eve.text(
             "Evening: " +
-            Math.floor((onecall.daily[i].temp.eve - 273.15) * 1.8 + 32) +
-            "°F"
+              Math.floor((onecall.daily[i].temp.eve - 273.15) * 1.8 + 32) +
+              "°F"
           );
 
           const humid = $('<p class="font-weight-bold">');
@@ -141,12 +137,10 @@ $(document).ready(function () {
 
   function geoFindMe() {
     function success(position) {
-      console.log("hi i'm paul");
-      console.log(position);
       basicData(
         "lat=" + position.coords.latitude + "&lon=" + position.coords.longitude
       );
-      console.log("i'm praying", basicURL);
+
       weatherAjax();
     }
 
@@ -167,7 +161,6 @@ $(document).ready(function () {
   $("#searchBtn").on("click", function () {
     event.preventDefault();
     basicData("q=" + $("#searchSpace").val());
-    console.log($("#searchSpace").val())
     weatherAjax();
   });
 
@@ -175,39 +168,8 @@ $(document).ready(function () {
     url: historyURL,
     method: "GET",
   }).then(function (response) {
-    console.log(response);
     $("#history").text(response);
   });
-
-  // // Breaking News API
-  // const newsurl =
-  //   "https://api.breakingapi.com/news?q=climate&type=headlines&locale=en-US&api_key=C6837518F5EC47FDB49E6D82FB5EE015";
-  // $.ajax({
-  //   url: newsurl,
-  //   method: "GET",
-  // }).then(function (newsStuff) {
-   
-  //   console.log(newsStuff);
-  //   for (let i = 0; i < 4; i++) {
-  //     const row = $("<ul>");
-  //     row.addClass("list-group list-group-flush")
-  //     const col = $("<li>");
-  //     col.addClass("list-group-item lead")
-  //     const articleBasic = $("<a>");
-  //     const link = newsStuff.articles[i].link;
-  //     articleBasic.attr('href', link);
-  //     articleBasic.attr('target', "_blank");
-  //     articleBasic.text(
-  //       newsStuff.articles[i].source.name + ": " + newsStuff.articles[i].title
-  //     );
-  //     const articleSnippet = $("<p>");
-  //     articleSnippet.text(newsStuff.articles[i].snippet);
-  //     col.append(articleBasic, articleSnippet);
-  //     row.append(col);
-  //     $("#newsSection").append(row);
-  //   }
-  // });
-
 
   // GNews API
   const newsurl =
@@ -216,16 +178,15 @@ $(document).ready(function () {
     url: newsurl,
     method: "GET",
   }).then(function (newsStuff) {
-    console.log('This is the news', newsStuff);
     for (let i = 0; i < 4; i++) {
       const row = $("<ul>");
-      row.addClass("list-group list-group-flush")
+      row.addClass("list-group list-group-flush");
       const col = $("<li>");
-      col.addClass("list-group-item lead")
+      col.addClass("list-group-item lead");
       const articleBasic = $("<a>");
       const link = newsStuff.articles[i].url;
-      articleBasic.attr('href', link);
-      articleBasic.attr('target', "_blank");
+      articleBasic.attr("href", link);
+      articleBasic.attr("target", "_blank");
       articleBasic.text(
         newsStuff.articles[i].source.name + ": " + newsStuff.articles[i].title
       );
@@ -234,6 +195,6 @@ $(document).ready(function () {
       col.append(articleBasic, articleSnippet);
       row.append(col);
       $("#newsSection").append(row);
-    };
+    }
   });
 });
